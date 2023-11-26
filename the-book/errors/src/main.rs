818e04932a72267ce7662d882c1029c9cf94f123
David Vogel <david@davidvogel.xyz>
2023-11-26 18:08:15 -0500

    "The Book", from 2023 November 26
    added code that was written while working through the "The Book":
    - Errors
    - Generics
    - Adder

---
+++ the-book/errors/src/main.rs
@@ -0,0 +1,10 @@
+use std::fs;
+use std::io;
+
+fn main() {
+    fn read_username_from_file() -> Result<String, io::Error> {
+        fs::read_to_string("hello.txt")
+    }
+
+    read_username_from_file;
+}
